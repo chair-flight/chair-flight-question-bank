16b70d0a2e2f0f24356e36b234c9274c8f485de9
Pedro Pupo Sa da Costa <pedro@hublo.com>
2022-06-01 23:45:52 +0100

    chore: release v0.1.4

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,26 @@
 import copy from "rollup-plugin-copy";
-import merge from "deepmerge";
-import { createBasicConfig } from "@open-wc/building-rollup";
+import typescript from "rollup-plugin-ts";
+import { terser } from "rollup-plugin-terser";
 
-const baseConfig = createBasicConfig();
-
-export default merge(baseConfig, {
-  input: "./lib-tsc-out/index.js",
-  output: {
-    dir: "lib",
-  },
+export default {
+  input: "./src/index.ts",
   plugins: [
+    typescript(),
+    terser(),
     copy({
       targets: [{ src: "pages/images/*", dest: "lib/images" }],
     }),
   ],
-});
+  output: [
+    {
+      file: "./lib/index.esm.js",
+      format: "esm",
+      exports: "auto",
+    },
+    {
+      file: "./lib/index.js",
+      format: "cjs",
+      exports: "auto",
+    },
+  ],
+};
